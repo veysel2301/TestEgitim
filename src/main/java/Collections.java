7aa7f5566fc9fa4ae0e8e981fd192d54525f5327
Veysel <veysel.oner@testinium.com>
2022-05-13 16:55:36 +0300

    Hesap makinesi örn

---
+++ src/main/java/Collections.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+public class Collections {
+    public static void main(String[] args) {
+//        int[]a={1,2,3,4};
+//        int[]arr=new int[3];
+//        arr[0]=3;
+//        System.out.println(a[0]);
+//        Ogrenci[] sinif =new Ogrenci[3];
+//        Ogrenci veysel =new Ogrenci();
+//        sinif[0]=new Ogrenci();
+//        sinif[1]=veysel;
+//        for (Ogrenci ogr:sinif){
+//            System.out.println(ogr.ad);
+//        }
+//        int[][]arr=new int[3][3];
+//        arr[0][0]=3;
+////
+//        int[]a={1,2,3,4,-44};
+//      Arrays.sort(a);
+//        for (int b :a) {
+//            System.out.print(b+" ");
+//
+//
+//        }
+      /*  ArrayList x =new ArrayList();
+        x.add("5");
+        x.add("6");
+        x.add("7");
+        x.remove(1);
+        System.out.println(x);
+        //liste.get(i ile erişim sağlayabiliriz.
+        //hasNext hala bir sonraki varsa
+        generic arraylist<String>
+        map ikili şeyleri tutmaya yarar.
+        */
+       //Map<K,V> mapte put ile eklenir indexle ulaşamıyon map lere keylerle ulaşılır. .put("1","pzt"); 1 key value
+ // set tekil değer taşır generic artık tek yapı tutuyor set ile
+        // length field parantez olmaz
+        // contains var mı yok mu boolean değer döndürür içinde prm göre
+        //Iterator
+    }
+
+}
