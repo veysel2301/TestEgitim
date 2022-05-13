3c2c1e768d20f7e3710828c2ceb4e397f4605bc6
Veysel <veysel.oner@testinium.com>
2022-05-13 14:13:12 +0300

    break ve continue

---
+++ src/main/java/EduThird.java
@@ -0,0 +1,30 @@
+public class EduThird {
+    public static void main(String[] args) {
+// for (int i=1;i<10;i++){
+//     if(i==5){
+//         break;
+//     }
+//     System.out.println(i);
+// }
+//        test:
+//        for (int i=1;i<10;i++){
+//            if(i==5){
+//                continue test;
+//            }
+//            System.out.println(i);// 5 İ ATLAR
+//        }
+//        test2:
+//        for (int i=1;i<100;i++){
+//            if (i%2==0){
+//                break test2;
+//            }
+//            System.out.println(i);
+//        }
+        for (int j=1;j<100;j++){
+            if (j % 4 == 0) {
+               continue;
+            }
+            System.out.println(j);
+        }
+    }
+}
